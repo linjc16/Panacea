5998e17ccb9310009706ff4606aff81e43646843
linjc16 <250551019@qq.com>
2024-03-11 13:39:18 -0500

    update eval of multi-sum

--- scripts/eval/summarization/multi/llama2-70b.sh
+++ scripts/eval/summarization/multi/llama2-70b.sh
@@ -1,10 +1,10 @@
-MODEL_PATH=openchat/openchat-3.5-0106
+MODEL_PATH=NousResearch/Llama-2-70b-chat-hf
 CACHE_DIR=/data/linjc/hub
 FILE_DIR=data/downstream/summazization/multi-trial
 SAVE_DIR=data/downstream/summazization/multi-trial/results
-MODEL_NAME=openchat-7b
+MODEL_NAME=llama2-70b
 
-CUDA_VISIBLE_DEVICES=0 python src/eval/summarization/multi/eval.py \
+CUDA_VISIBLE_DEVICES=3,6 python src/eval/summarization/multi/eval.py \
     --model_path $MODEL_PATH \
     --cache_dir $CACHE_DIR \
     --file_dir $FILE_DIR \
--- src/eval/summarization/multi/eval.py
+++ src/eval/summarization/multi/eval.py
@@ -13,7 +13,7 @@ tqdm.pandas()
 def load_model(model_path, cache_dir):
     tokenizer = AutoTokenizer.from_pretrained(model_path, cache_dir=cache_dir, padding_side='left')
     tokenizer.pad_token = tokenizer.eos_token
-    tokenizer.model_max_length = 1000000000000000019884624838656
+    # tokenizer.model_max_length = 1000000000000000019884624838656
     
     model = AutoModelForCausalLM.from_pretrained(
         model_path, cache_dir=cache_dir,
@@ -81,80 +81,79 @@ if __name__ == '__main__':
         with open(os.path.join(args.save_dir, f'{args.model_name}.csv'), 'w') as f:
             f.write('id,summary\n')
 
-    
-    batch_size = 1
-    # transform the df into batches, each batch has ids and study_text
-    df = df.groupby(df.index // batch_size).agg({'id': lambda x: list(x), 'study_text': lambda x: list(x), 'background': lambda x: list(x)}) 
-    
-    
     for i in tqdm(range(len(df))):
         row = df.iloc[i]
-        ids = row['id']
-        study_text = row['study_text']
-        background = row['background']
+        id = row['id']
+        input_text = row['study_text']
+        bg_text = row['background']
         
-        merged_input_text = []
-        for input_text, bg_text in zip(study_text, background):
-            merged_input_text.append(instruction_prompt.format(Text=input_text, Background=bg_text))
+        merged_input_text = instruction_prompt.format(Text=input_text, Background=bg_text)
 
-        # tokenize the input_text
-        inputs = tokenizer(merged_input_text, padding=True, return_tensors="pt")
-        inputs = inputs.to(model.device)
-
-        # pdb.set_trace()
-
-        with torch.no_grad():
-            generated_ids = model.generate(**inputs, max_new_tokens=512, do_sample=False)
-
-        summaries = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
-
-        predictions = []
-        for summary in summaries:
-            # for the generated text, remove the input_text, find its index, and get the text after it
-            try:
-                summary = summary[summary.find('Summary:') + len('Summary:'):]
-            except:
-                pdb.set_trace()
-                summary = ""
-            
-            predictions.append(summary.strip())
-
-        # save id and prediction to dataframe
-        for j in range(len(ids)):
-            results = pd.DataFrame(columns=['id', 'summary'])
-            id = ids[j]
-            prediction = predictions[j]
-            
-            # add id and prediction to a row
-            results.loc[j] = [id, prediction]
-            results.to_csv(os.path.join(args.save_dir, f'{args.model_name}.csv'), mode='a', header=False, index=False)
+        messages = [
+            {"role": "user", "content": merged_input_text},
+        ]
+        
+        encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt").to(model.device)
+        generated_ids = model.generate(encodeds, max_new_tokens=512, do_sample=False)
+        summary = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        
+        try:
+            prediction = summary[summary.find('Summary:') + len('Summary:'):]
+        except:
+            prediction = ""
+        prediction = prediction.strip()
+        
+        results = pd.DataFrame(columns=['id', 'summary'])
+        
+        # add id and prediction to a row
+        results.loc[0] = [id, prediction]
+        results.to_csv(os.path.join(args.save_dir, f'{args.model_name}.csv'), mode='a', header=False, index=False)
+    
+    
+    # batch_size = 1
+    # transform the df into batches, each batch has ids and study_text
+    # df = df.groupby(df.index // batch_size).agg({'id': lambda x: list(x), 'study_text': lambda x: list(x), 'background': lambda x: list(x)}) 
+    
     
     # for i in tqdm(range(len(df))):
     #     row = df.iloc[i]
-    #     id = row['id']
-    #     input_text = row['input_text']
+    #     ids = row['id']
+    #     study_text = row['study_text']
+    #     background = row['background']
         
-    #     pdb.set_trace()
+    #     merged_input_text = []
+    #     for input_text, bg_text in zip(study_text, background):
+    #         merged_input_text.append(instruction_prompt.format(Text=input_text, Background=bg_text))
 
-    #     merged_input_text = instruction_prompt.format(Text=input_text)
+    #     # tokenize the input_text
+    #     inputs = tokenizer(merged_input_text, padding=True, return_tensors="pt")
+    #     inputs = inputs.to(model.device)
 
-    #     messages = [
-    #         {"role": "user", "content": merged_input_text},
-    #     ]
-        
-    #     encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt").to(model.device)
-    #     generated_ids = model.generate(encodeds, max_new_tokens=512, do_sample=False)
-    #     summary = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-        
-    #     try:
-    #         prediction = summary[summary.find('Summary:') + len('Summary:'):]
-    #     except:
-    #         prediction = ""
-    #     prediction = prediction.strip()
-        
-    #     results = pd.DataFrame(columns=['id', 'summary'])
-        
-    #     # add id and prediction to a row
-    #     results.loc[0] = [id, prediction]
-    #     results.to_csv(os.path.join(args.save_dir, f'{args.model_name}.csv'), mode='a', header=False, index=False)
+    #     # pdb.set_trace()
+
+    #     with torch.no_grad():
+    #         generated_ids = model.generate(**inputs, max_new_tokens=512, do_sample=False)
+
+    #     summaries = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+
+    #     predictions = []
+    #     for summary in summaries:
+    #         # for the generated text, remove the input_text, find its index, and get the text after it
+    #         try:
+    #             summary = summary[summary.find('Summary:') + len('Summary:'):]
+    #         except:
+    #             pdb.set_trace()
+    #             summary = ""
+            
+    #         predictions.append(summary.strip())
+
+    #     # save id and prediction to dataframe
+    #     for j in range(len(ids)):
+    #         results = pd.DataFrame(columns=['id', 'summary'])
+    #         id = ids[j]
+    #         prediction = predictions[j]
+            
+    #         # add id and prediction to a row
+    #         results.loc[j] = [id, prediction]
+    #         results.to_csv(os.path.join(args.save_dir, f'{args.model_name}.csv'), mode='a', header=False, index=False)
     
