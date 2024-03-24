52d356bc992ba6c693a262f443dad473f671f917
linjc16 <250551019@qq.com>
2024-03-24 09:55:54 -0500

    update

---
+++ scripts/eval/matching/patient2trial/panacea-ft.sh
@@ -0,0 +1,13 @@
+MODEL_PATH=
+CACHE_DIR=/data/linjc/hub
+DATASET=cohort
+SAVE_DIR=data/downstream/matching/patient2trial/$DATASET/results
+MODEL_NAME=panacea-ft
+
+CUDA_VISIBLE_DEVICES=4 python src/eval/matching/patient2trial/eval.py \
+    --model_path $MODEL_PATH \
+    --cache_dir $CACHE_DIR \
+    --dataset $DATASET \
+    --save_dir $SAVE_DIR \
+    --model_name $MODEL_NAME \
+    --split test
