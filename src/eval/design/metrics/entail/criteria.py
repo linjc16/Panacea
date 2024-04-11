7d0967b2ec2e29789459164dee3189af4281d4e9
linjc16 <250551019@qq.com>
2024-04-10 20:19:58 -0500

    update plot entail

--- src/eval/design/metrics/entail/criteria.py
+++ src/eval/design/metrics/entail/criteria.py
@@ -23,40 +23,40 @@ if __name__ == "__main__":
         results = json.load(f)
 
 
-    # instruction_prompts = (
-    #     "Act as an impartial judge and evaluate whether the criteria mentioned in a model's output are present in the full list of the groundtruth criteria. "
-    #     "Output '1' or '0', where '1' means the criteria mentioned in the model's output are fully included in the groundtruth criteria list, and '0' means the criteria from the model's output are not included in the groundtruth. "
-    #     "You should provide an explanation for the evaluation. "
-    #     "\n\n"
-    #     "Example:\n"
-    #     "[Model Output]\nExcellent! Moving on to the third criterion, I propose \"Ability to provide written informed consent.\" Informed consent is a fundamental ethical requirement in clinical research. Participants must fully understand the trial and voluntarily agree to participate.\n[End of Model Output]\n"
-    #     "[Groundtruth Criteria list]\nInclusion Criteria:~Age between 18 and 120 years at time of consent~Ability to speak and understand English~Clinical stage I, II or IIIa NSCLC~Candidate for RTS segmentectomy, as determined by the operating surgeon~Exclusion Criteria:~Anticoagulation with inability to cease anticoagulant therapy prior to surgery~Incurable coagulopathy~Systemic vascular disease or vasculitis~Not a candidate for RTS segmentectomy\n[End of Groundtruth Criteria]\n"
-    #     "Match prediction: 0"
-    #     "\n\n"
-    #     "Now, evaluate the following model output and groundtruth criteria list. "
-    #     "You should first output the 'match prediction' at the beginning of the response by `Match prediction: `, e.g., `Match prediction: 1`. Then, Provide an explanation for your evaluation. "
-    #     "\n\n"
-    #     "[Model Output]\n{model_output}\n[End of Model Output]\n\n"
-    #     "[Groundtruth Criteria list]\n{groundtruth}\n[End of Groundtruth Criteria]"
-    # )
-
     instruction_prompts = (
-        "Act as an impartial judge and evaluate whether the input model's output match the groundtruth. "
-        "Output '1' or '0', where '1' means the criteria mentioned in the model's output are relevant to the groundtruth, and '0' means the criteria from the model's output are not relevant to the groundtruth. "
-        "Note that you can allow that the model's output is not exactly the same as the groundtruth, but it should be relevant to the groundtruth. "
+        "Act as an impartial judge and evaluate whether the criteria mentioned in a model's output are present in the full list of the groundtruth criteria. "
+        "Output '1' or '0', where '1' means the criteria mentioned in the model's output are fully included in the groundtruth criteria list, and '0' means the criteria from the model's output are not included in the groundtruth. "
         "You should provide an explanation for the evaluation. "
         "\n\n"
         "Example:\n"
         "[Model Output]\nExcellent! Moving on to the third criterion, I propose \"Ability to provide written informed consent.\" Informed consent is a fundamental ethical requirement in clinical research. Participants must fully understand the trial and voluntarily agree to participate.\n[End of Model Output]\n"
-        "[Groundtruth]\nPerfect. Now, the third criterion should be \"Persistent AF (atrial fibrillation lasting >7 days) of total continuous duration <2 years as documented in medical notes.\" This criterion ensures that the study participants have a consistent and recent history of persistent AF, which is necessary for accurately assessing the effectiveness of the interventions on this specific condition. What are your thoughts?\n[End of Groundtruth]\n"
+        "[Groundtruth Criteria list]\nInclusion Criteria:~Age between 18 and 120 years at time of consent~Ability to speak and understand English~Clinical stage I, II or IIIa NSCLC~Candidate for RTS segmentectomy, as determined by the operating surgeon~Exclusion Criteria:~Anticoagulation with inability to cease anticoagulant therapy prior to surgery~Incurable coagulopathy~Systemic vascular disease or vasculitis~Not a candidate for RTS segmentectomy\n[End of Groundtruth Criteria]\n"
         "Match prediction: 0"
         "\n\n"
-        "Now, evaluate the following model output and groundtruth. "
+        "Now, evaluate the following model output and groundtruth criteria list. "
         "You should first output the 'match prediction' at the beginning of the response by `Match prediction: `, e.g., `Match prediction: 1`. Then, Provide an explanation for your evaluation. "
         "\n\n"
         "[Model Output]\n{model_output}\n[End of Model Output]\n\n"
-        "[Groundtruth]\n{groundtruth}\n[End of Groundtruth]"
+        "[Groundtruth Criteria list]\n{groundtruth}\n[End of Groundtruth Criteria]"
     )
+
+    # instruction_prompts = (
+    #     "Act as an impartial judge and evaluate whether the input model's output match the groundtruth. "
+    #     "Output '1' or '0', where '1' means the criteria mentioned in the model's output are relevant to the groundtruth, and '0' means the criteria from the model's output are not relevant to the groundtruth. "
+    #     "Note that you can allow that the model's output is not exactly the same as the groundtruth, but it should be relevant to the groundtruth. "
+    #     "You should provide an explanation for the evaluation. "
+    #     "\n\n"
+    #     "Example:\n"
+    #     "[Model Output]\nExcellent! Moving on to the third criterion, I propose \"Ability to provide written informed consent.\" Informed consent is a fundamental ethical requirement in clinical research. Participants must fully understand the trial and voluntarily agree to participate.\n[End of Model Output]\n"
+    #     "[Groundtruth]\nPerfect. Now, the third criterion should be \"Persistent AF (atrial fibrillation lasting >7 days) of total continuous duration <2 years as documented in medical notes.\" This criterion ensures that the study participants have a consistent and recent history of persistent AF, which is necessary for accurately assessing the effectiveness of the interventions on this specific condition. What are your thoughts?\n[End of Groundtruth]\n"
+    #     "Match prediction: 0"
+    #     "\n\n"
+    #     "Now, evaluate the following model output and groundtruth. "
+    #     "You should first output the 'match prediction' at the beginning of the response by `Match prediction: `, e.g., `Match prediction: 1`. Then, Provide an explanation for your evaluation. "
+    #     "\n\n"
+    #     "[Model Output]\n{model_output}\n[End of Model Output]\n\n"
+    #     "[Groundtruth]\n{groundtruth}\n[End of Groundtruth]"
+    # )
 
     # data/downstream/design/raw/selected_step1/merged/test/merged.json
     with open('data/downstream/design/raw/selected_step1/merged/test/merged.json', 'r') as f:
@@ -82,8 +82,8 @@ if __name__ == "__main__":
         model_output = value['model_response']
         if del_end_sent[key]:
             model_output = model_output[:-1]
-        # groundtruth = test_data_dict[key]['eligibility_criteria']
-        groundtruth = value['groundtruth']
+        groundtruth = test_data_dict[key]['eligibility_criteria']
+        # groundtruth = value['groundtruth']
         
         model_preds = []
 
---
+++ src/eval/design/metrics/plot/entail.py
@@ -0,0 +1,64 @@
+import json
+import argparse
+import re
+import os
+import pdb
+import glob
+from collections import defaultdict
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--res_dir', type=str, default='data/downstream/design/results/criteria/eval_entail_v1')
+    args = parser.parse_args()
+
+    filepaths = glob.glob(os.path.join(args.res_dir, f'*.json'))
+
+    final_results = defaultdict(dict)
+    for filepath in tqdm(filepaths):
+        model_name = os.path.basename(filepath).split('_')[0]
+        with open(filepath, 'r') as f:
+            results = json.load(f)
+        
+        back_steps = range(-4, 0)
+        for back_step in back_steps:
+            evals = []
+            for key, value in results.items():
+                eval_res_list = value['eval_results']
+                # extract the number after "Match prediction: " from the list
+                match_pred = []
+                for eval_res in eval_res_list[back_step:]:
+                    try:
+                        pred = re.findall(r'Match prediction: (\d)', eval_res)
+                        match_pred.append(int(pred[0]))
+                    except:
+                        match_pred.append(0)
+
+                evals.extend(match_pred)
+        
+            # calculate the accuracy
+            accuracy = sum(evals) / len(evals)
+
+            final_results[back_step][model_name] = accuracy
+    
+    # plot the results, x axis is the back steps, y axis is the accuracy for each model
+
+    back_steps = list(final_results.keys())
+    models = list(final_results[-1].keys())
+
+    plt.figure(figsize=(10, 6))
+
+    for model in models:
+        accuracies = [final_results[step][model] for step in back_steps]
+        plt.plot(back_steps, accuracies, label=model, marker='o')
+
+    plt.xlabel('Back Steps')
+    plt.ylabel('Accuracy')
+    plt.title('Model Accuracy Over Back Steps')
+    plt.legend()
+    plt.grid(True)
+    plt.xticks(back_steps)
+    plt.savefig('./entail.png', dpi=300)
