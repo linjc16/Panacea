52d7c6ea69a48ac78fff368070cdeeb62fa77120
linjc16 <250551019@qq.com>
2024-05-03 09:08:08 -0500

    update mesh tags

--- src/vis/analysis/mesh/crawl_mesh_terms.py
+++ src/vis/analysis/mesh/crawl_mesh_terms.py
@@ -5,41 +5,55 @@ import pdb
 # from Bio import Entrez
 import re
 import multiprocessing as mp
-from tqdm.contrib.concurrent import process_map
 import pubmed_parser as pp
 import json
 import random
 import time
+import multiprocessing as mp
+from tqdm import tqdm
 
 from xml.etree import ElementTree as ET
 import requests
 
-save_dir = 'data/analysis/icd10/mesh/'
+save_dir = 'data/analysis/icd10/mesh/raw'
+os.makedirs(save_dir, exist_ok=True)
 
-def get_pmc_id_from_pubmed(pubmed_id):
+def get_pmc_id_from_pubmed(pubmed_id, process_id):
     mesh_details = []
 
-    try:
-        response = requests.get(f'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id={pubmed_id}&retmode=xml&rettype=medline')
-        tree = ET.fromstring(response.content)
-        for mesh in tree.findall('.//MeshHeading'):
-            descriptor = mesh.find('DescriptorName')
-            if descriptor is not None:
-                ui = descriptor.attrib['UI']
-                term = descriptor.text
-                mesh_details.append((ui, term))
+    attempts = 0
+    while attempts < 100:
+        if pubmed_id == '22382203':
+            pdb.set_trace()
+        try:
+            response = requests.get(f'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id={pubmed_id}&retmode=xml&rettype=medline')
+            tree = ET.fromstring(response.content)
+            for mesh in tree.findall('.//MeshHeading'):
+                descriptor = mesh.find('DescriptorName')
+                if descriptor is not None:
+                    ui = descriptor.attrib['UI']
+                    term = descriptor.text
+                    mesh_details.append((ui, term))
 
-        with open(os.path.join(save_dir, 'mesh_terms.json'), 'a') as f:
-            f.write(json.dumps({'pubmed_id': pubmed_id, 'mesh_terms': mesh_details}) + '\n')
+            with open(os.path.join(save_dir, f'mesh_terms_{process_id}.json'), 'a') as f:
+                f.write(json.dumps({'pubmed_id': pubmed_id, 'mesh_terms': mesh_details}) + '\n')
+            
+            time.sleep(random.randint(1, 3)/10)
+            return 
+
+        except:
+            attempts += 1
         
-        time.sleep(random.randint(1, 3)/10)
-
-    except:
-        return
+    print(f'Error with {pubmed_id}')
 
 
-def process_row(pubmed_id):
-    return get_pmc_id_from_pubmed(pubmed_id)
+def process_row(input):
+    df, process_id = input
+
+    for index, row in tqdm(df.iterrows(), total=len(df)):
+        pubmed_id = row['pubmed_id']
+        get_pmc_id_from_pubmed(pubmed_id, process_id)
+    
 
 
 if __name__ == '__main__':
@@ -51,21 +65,15 @@ if __name__ == '__main__':
         df_list.append(df)
     
     df = pd.concat(df_list)
-
-    # df = df.iloc[:10]
-
-    # for each row, get the pmc id
-    # df['pmc_id'] = df['pubmed_id'].apply(lambda x: get_pmc_id_from_pubmed(x))
     
-    pmc_ids = process_map(process_row, df['pubmed_id'], max_workers=16)
-    # process_row(df['pubmed_id'])
-
-    # pdb.set_trace()
-
-    # # count the number of papers with pmc id
-    # print('There are {} papers with pmc id'.format(len(df[df['pmc_id'].notnull()])))
+    num_processes = 2
     
-    # save_dir = '3_crawl_paper/pubmed/data/raw_pmc/pubmed_raw.csv'
-    # os.makedirs(os.path.dirname(save_dir), exist_ok=True)
-    # # save the df to 3_crawl_paper/pubmed/data/raw_pmc/
-    # df.to_csv(save_dir, index=False)
+    chunk_size = int(len(df)/num_processes)
+    chunks = [df.iloc[i:i + chunk_size] for i in range(0, len(df), chunk_size)]
+
+    inputs = [(chunk, i) for i, chunk in enumerate(chunks)]
+
+    with mp.Pool(num_processes) as pool:
+        pool.map(process_row, inputs)
+
+    # process_row(inputs[0])
