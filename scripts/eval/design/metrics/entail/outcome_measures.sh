216ba9e226b1eaea5ba7ab7e713345aabd6eac80
linjc16 <250551019@qq.com>
2024-05-16 01:32:44 -0500

    update

--- scripts/eval/design/metrics/entail/outcome_measures.sh
+++ scripts/eval/design/metrics/entail/outcome_measures.sh
@@ -1,4 +1,9 @@
 MODEL_NAME=$1
 
-python src/eval/design/metrics/entail/outcome_measures.py \
-    --model_name $MODEL_NAME \
+for EXP_ID in 1 2
+do
+    RES_DIR=data/downstream/design/results$EXP_ID/outcome_measures
+    python src/eval/design/metrics/entail/outcome_measures.py \
+        --model_name $MODEL_NAME \
+        --res_dir $RES_DIR
+done
--- scripts/eval/design/metrics/entail/study_arms.sh
+++ scripts/eval/design/metrics/entail/study_arms.sh
@@ -1,4 +1,9 @@
 MODEL_NAME=$1
 
-python src/eval/design/metrics/entail/study_arms.py \
-    --model_name $MODEL_NAME \
+for EXP_ID in 1 2
+do
+    RES_DIR=data/downstream/design/results$EXP_ID/study_arms
+    python src/eval/design/metrics/entail/study_arms.py \
+        --model_name $MODEL_NAME \
+        --res_dir $RES_DIR
+done
