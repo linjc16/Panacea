97ea6f18799e72201a092aadc83a68b1a5199d39
linjc16 <250551019@qq.com>
2024-05-16 22:16:10 -0500

    update

--- scripts/eval/design/metrics/rouge.sh
+++ scripts/eval/design/metrics/rouge.sh
@@ -1,6 +1,7 @@
 MODEL_NAME=$1
 TASK=$2
-RES_DIR=data/downstream/design/results/$TASK
+EXP_ID=$3
+RES_DIR=data/downstream/design/results$EXP_ID/$TASK
 
 python src/eval/design/metrics/rouge.py \
     --model_name $MODEL_NAME \
