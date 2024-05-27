96a3a635609ff642986a4ecd1410c41240b0f20b
linjc16 <250551019@qq.com>
2024-05-27 16:12:14 -0500

    update

--- src/eval/summarization/single/metrics/gpt_scores.py
+++ src/eval/summarization/single/metrics/gpt_scores.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     parser.add_argument('--res_dir', type=str, default='data/downstream/summazization/single-trial/results/gpt_eval')
     parser.add_argument('--model_name', type=str, default='llama2')
     args = parser.parse_args()
-    
+
     # read json file
     with open (os.path.join(args.res_dir, f'{args.model_name}.json'), 'r') as f:
         preds = json.load(f)
@@ -59,6 +59,6 @@ if __name__ == "__main__":
     print(f'Model: {args.model_name}, Goal Alignment: {goal_score:.4f}, Patient Recruiting Method: {recruiting_score:.4f}, Study Arm Consistency: {study_arm_score:.4f}, Conclusion Similarity: {conclusion_score:.4f}')
 
     # calculate all scores
-    avg_scores = (goal_score + recruiting_score + study_arm_score + conclusion_score) / 4
+    avg_scores = (goal_score + recruiting_score + study_arm_score) / 3
 
     print(f'Model: {args.model_name}, Average Score: {avg_scores:.4f}')
