923ea5d0afb0453724b049c27c930b7112aecb9f
linjc16 <250551019@qq.com>
2024-05-29 11:57:13 -0500

    update

--- scripts/eval/design/sequential/entail/study_arms.sh
+++ scripts/eval/design/sequential/entail/study_arms.sh
@@ -1,6 +1,6 @@
 MODEL_NAME=$1
 
-RES_DIR=data/downstream/design/results$EXP_ID/sequential/study_arms
+RES_DIR=data/downstream/design/results/sequential/study_arms
 python src/eval/design/metrics/entail/study_arms.py \
     --model_name $MODEL_NAME \
     --res_dir $RES_DIR
--- src/eval/summarization/multi/metrics/gpt_scores.py
+++ src/eval/summarization/multi/metrics/gpt_scores.py
@@ -52,13 +52,15 @@ if __name__ == "__main__":
             conclusion_scores.append(1)
     
     goal_score = sum(goal_scores) / len(goal_scores)
-    recruiting_score = sum(recruiting_scores) / len(recruiting_scores)
-    study_arm_score = sum(study_arm_scores) / len(study_arm_scores)
-    conclusion_score = sum(conclusion_scores) / len(conclusion_scores)
+    # recruiting_score = sum(recruiting_scores) / len(recruiting_scores)
+    # study_arm_score = sum(study_arm_scores) / len(study_arm_scores)
+    # conclusion_score = sum(conclusion_scores) / len(conclusion_scores)
 
-    print(f'Model: {args.model_name}, Goal Alignment: {goal_score:.4f}, Patient Recruiting Method: {recruiting_score:.4f}, Study Arm Consistency: {study_arm_score:.4f}, Conclusion Similarity: {conclusion_score:.4f}')
+    print(f'Model: {args.model_name}, Goal Alignment: {goal_score:.4f}')
 
-    # calculate all scores
-    avg_scores = (goal_score + recruiting_score + study_arm_score) / 3
+    # print(f'Model: {args.model_name}, Goal Alignment: {goal_score:.4f}, Patient Recruiting Method: {recruiting_score:.4f}, Study Arm Consistency: {study_arm_score:.4f}, Conclusion Similarity: {conclusion_score:.4f}')
 
-    print(f'Model: {args.model_name}, Average Score: {avg_scores:.4f}')
+    # # calculate all scores
+    # avg_scores = (goal_score + recruiting_score + study_arm_score) / 3
+
+    # print(f'Model: {args.model_name}, Average Score: {avg_scores:.4f}')
