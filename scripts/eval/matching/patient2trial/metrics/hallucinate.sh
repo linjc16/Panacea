cbbb7b9f36ceb3578fc01727582a5eecd3738fa3
linjc16 <250551019@qq.com>
2024-06-01 16:32:00 -0500

    update

--- scripts/eval/matching/patient2trial/metrics/hallucinate.sh
+++ scripts/eval/matching/patient2trial/metrics/hallucinate.sh
@@ -4,7 +4,6 @@ EXP_ID=$3
 RES_DIR=data/downstream/matching/patient2trial/$DATASET/results$EXP_ID
 
 
-
 python src/vis/analysis/downstream/matching/hallucination/eval_hall.py \
     --model_name $MODEL_NAME \
     --dataset $DATASET \
--- src/vis/analysis/downstream/matching/hallucination/eval_hall.py
+++ src/vis/analysis/downstream/matching/hallucination/eval_hall.py
@@ -3,6 +3,7 @@ import re
 import os
 import argparse
 import pdb
+from tqdm import tqdm
 
 import sys
 sys.path.append('./')
@@ -13,7 +14,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_name', type=str, default='llama2')
     parser.add_argument('--res_dir', type=str, default='/data/linjc/trialfm/downstream/summarization/results')
-    parser.add_argument('--dataset', type=str, default='cohort')
+    parser.add_argument('--dataset', type=str, default='TREC2021')
     args = parser.parse_args()
 
     args.res_dir = os.path.join(args.res_dir, f'{args.model_name}.json')
@@ -40,7 +41,13 @@ if __name__ == '__main__':
     
     assert len(preds) == len(labels)
     
-
+    if args.dataset == 'TREC2021':
+        label_mapping = {
+            0: "0) Excluded (patient meets inclusion criteria, but is excluded on the grounds of the trial's exclusion criteria)",
+            1: "1) Not relevant (patient does not have sufficient information to qualify for the trial)",
+            2: "2) Eligible (patient meets inclusion criteria and exclusion criteria do not apply)"
+        }
+    
     prompt = (
         'Given a patient note, clinical tiral eligibility criteria, and a label, help me understand why the model made a wrong prediction. '
         'You can choose one or more reasons from the following list: '
@@ -51,17 +58,48 @@ if __name__ == '__main__':
         'Clinical trial eligibility criteria: {criteria}\n'
         'Label: {label}\n'
         'Model prediction: {pred}\n\n'
-        'Output the error reason in a list format, e.g., [1] or [1, 2].\n\n'
+        'Output the error reason in a list format in the last line, e.g., Reason type: [1] or [1, 2].\n\n'
     )
 
+    with open('src/vis/analysis/downstream/matching/hallucination/inputs.json', 'r') as f:
+        inputs = json.load(f)
+
     reasons_dict = {}
-    for i in range(len(preds)):
+    i = 0
+    for i in tqdm(range(len(preds))):
         if preds[i] == -1:
             reasons_dict[i] = 'Non-responsive Error'
+            if i % 100 == 0:
+                with open(f'src/vis/analysis/downstream/matching/hallucination/results/{args.model_name}.json', 'w') as f:
+                    json.dump(reasons_dict, f, indent=4)
+                
+            i += 1
             continue
-
+            
         if preds[i] != labels[i]:
+            patient_notes = inputs[str(i)]['patient_notes']
+            criteria = inputs[str(i)]['clinical_trial']
             model_output = results[str(i)]['output']
+            label = label_mapping[labels[i]]
 
-    # for each sample, if the pred is wrong, use chat_haiku to analize the error reason
+            prompt = prompt.format(patient_note=patient_notes, criteria=criteria, label=label, pred=model_output)
+            attempt = 0
+            while attempt < 10:
+                try:
+                    reason = chat_haiku(prompt)
+                    break
+                except:
+                    attempt += 1
+                    continue
+            
+            reasons_dict[i] = reason
+        
+        if i % 100 == 0:
+            with open(f'src/vis/analysis/downstream/matching/hallucination/results/{args.model_name}.json', 'w') as f:
+                json.dump(reasons_dict, f, indent=4)
+            
+        i += 1
+
+    with open(f'src/vis/analysis/downstream/matching/hallucination/results/{args.model_name}.json', 'w') as f:
+        json.dump(reasons_dict, f, indent=4)
     
